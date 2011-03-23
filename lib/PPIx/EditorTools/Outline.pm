cef6d4dc41b49f1e2a71a93fadae8694c364fc5c
Gabor Szabo <szabgab@gmail.com>
2011-03-23 12:51:00 +0000

    copy the content of Padre::Document::Perl::Outline to PPIx::Editortools::Outline to make it available to non-Padre uses as well

---
+++ lib/PPIx/EditorTools/Outline.pm
@@ -0,0 +1,169 @@
+package PPIx::EditorTools::Outline;
+
+# ABSTRACT: PPI-based outline builder
+
+use 5.008;
+use strict;
+use warnings;
+use Carp;
+
+use base 'PPIx::EditorTools';
+use Class::XSAccessor accessors => {};
+
+use PPI;
+
+=pod
+
+=head1 SYNOPSIS
+
+  my $outline = PPIx::EditorTools::Outline->new->find(
+        code => "package TestPackage;\nsub x { 1;\n"
+      );
+ print Dumper $outline;
+
+=head1 DESCRIPTION
+
+Return a list of pragmatas, modules, methods of a C<PPI::Document>.
+
+=head1 METHODS
+
+=over 4
+
+=item new()
+
+Constructor. Generally shouldn't be called with any arguments.
+
+=item find( ppi => PPI::Document $ppi )
+=item find( code => Str $code )
+
+Accepts either a C<PPI::Document> to process or a string containing
+the code (which will be converted into a C<PPI::Document>) to process.
+Return a reference to a hash.
+
+=back
+
+=cut
+
+
+sub find {
+	my ( $self, %args ) = @_;
+	$self->process_doc(%args);
+
+	my $ppi = $self->ppi;
+
+	return [] unless defined $ppi;
+	$ppi->index_locations;
+
+	# Search for interesting things
+	require PPI::Find;
+	my @things = PPI::Find->new(
+		sub {
+
+			# This is a fairly ugly search
+			return 1 if ref $_[0] eq 'PPI::Statement::Package';
+			return 1 if ref $_[0] eq 'PPI::Statement::Include';
+			return 1 if ref $_[0] eq 'PPI::Statement::Sub';
+			return 1 if ref $_[0] eq 'PPI::Statement';
+		}
+	)->in($ppi);
+
+	# Define a flag indicating that further Method::Signature/Moose check should run
+	my $check_alternate_sub_decls = 0;
+
+	# Build the outline structure from the search results
+	my @outline       = ();
+	my $cur_pkg       = {};
+	my $not_first_one = 0;
+	foreach my $thing (@things) {
+		if ( ref $thing eq 'PPI::Statement::Package' ) {
+			if ($not_first_one) {
+				if ( not $cur_pkg->{name} ) {
+					$cur_pkg->{name} = 'main';
+				}
+				push @outline, $cur_pkg;
+				$cur_pkg = {};
+			}
+			$not_first_one   = 1;
+			$cur_pkg->{name} = $thing->namespace;
+			$cur_pkg->{line} = $thing->location->[0];
+		} elsif ( ref $thing eq 'PPI::Statement::Include' ) {
+			next if $thing->type eq 'no';
+			if ( $thing->pragma ) {
+				push @{ $cur_pkg->{pragmata} }, { name => $thing->pragma, line => $thing->location->[0] };
+			} elsif ( $thing->module ) {
+				push @{ $cur_pkg->{modules} }, { name => $thing->module, line => $thing->location->[0] };
+				unless ($check_alternate_sub_decls) {
+					$check_alternate_sub_decls = 1
+						if grep { $thing->module eq $_ } (
+						'Method::Signatures',
+						'MooseX::Declare',
+						'MooseX::Method::Signatures'
+						);
+				}
+			}
+		} elsif ( ref $thing eq 'PPI::Statement::Sub' ) {
+			push @{ $cur_pkg->{methods} }, { name => $thing->name, line => $thing->location->[0] };
+		} elsif ( ref $thing eq 'PPI::Statement' ) {
+
+			# last resort, let's analyse further down...
+			my $node1 = $thing->first_element;
+			my $node2 = $thing->child(2);
+			next unless defined $node2;
+
+			# Moose attribute declaration
+			if ( $node1->isa('PPI::Token::Word') && $node1->content eq 'has' ) {
+				push @{ $cur_pkg->{attributes} }, { name => $node2->content, line => $thing->location->[0] };
+				next;
+			}
+
+			# MooseX::POE event declaration
+			if ( $node1->isa('PPI::Token::Word') && $node1->content eq 'event' ) {
+				push @{ $cur_pkg->{events} }, { name => $node2->content, line => $thing->location->[0] };
+				next;
+			}
+		}
+	}
+
+	if ($check_alternate_sub_decls) {
+		$ppi->find(
+			sub {
+				$_[1]->isa('PPI::Token::Word') or return 0;
+				$_[1]->content =~ /^(?:func|method)\z/ or return 0;
+				$_[1]->next_sibling->isa('PPI::Token::Whitespace') or return 0;
+				my $sib_content = $_[1]->next_sibling->next_sibling->content or return 0;
+
+				$sib_content =~ m/^\b(\w+)\b/;
+				return 0 unless defined $1;
+
+				push @{ $cur_pkg->{methods} }, { name => $1, line => $_[1]->line_number };
+
+				return 1;
+			}
+		);
+	}
+
+	if ( not $cur_pkg->{name} ) {
+		$cur_pkg->{name} = 'main';
+	}
+
+	push @outline, $cur_pkg;
+
+	return \@outline;
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+This class inherits from C<PPIx::EditorTools>.
+Also see L<App::EditorTools>, L<Padre>, and L<PPI>.
+
+=cut
+
+# Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
---
+++ t/09-outline.t
@@ -0,0 +1,141 @@
+#!/usr/bin/perl
+
+use strict;
+BEGIN {
+	$^W = 1;
+}
+
+use Test::More;
+use Test::Differences;
+use PPI;
+
+BEGIN {
+	if ($PPI::VERSION =~ /_/) {
+		plan skip_all => "Need released version of PPI. You have $PPI::VERSION";
+		exit 0;
+	}
+}
+
+my @cases = (
+	{
+		code => <<'END_CODE',
+use strict; use warnings;
+use Abc;
+
+my $global = 42;
+
+sub qwer {
+}
+
+END_CODE
+		expected => [
+		   {
+		     'methods' => [
+		       {
+		         'line' => 6,
+		         'name' => 'qwer'
+		       }
+		     ],
+		     'modules' => [
+		       {
+		         'line' => 2,
+		         'name' => 'Abc'
+		       }
+		     ],
+		     'name' => 'main',
+		     'pragmata' => [
+		       {
+		         'line' => 1,
+		         'name' => 'strict'
+		       },
+		       {
+		         'line' => 1,
+		         'name' => 'warnings'
+		       }
+		     ]
+		   }
+		 ],
+	},
+	{
+		code => <<'END_CODE',
+#!/usr/bin/perl
+
+use 5.008;
+use strict;
+use autodie;
+use warnings FATAL => 'all';
+
+use lib ('/opt/perl5/lib');
+
+my $global = 42;
+
+print "start";
+
+sub abc {
+   print 1;
+
+   my $private = 42;
+
+   sub def {
+   }
+   print 2;
+}
+
+print "ok";
+
+sub xyz { }
+
+print "end";
+
+END_CODE
+
+		expected => [
+			   {
+			     'methods' => [
+			       {
+			         'line' => 14,
+			         'name' => 'abc'
+			       },
+			       {
+			         'line' => 19,
+			         'name' => 'def'
+			       },
+			       {
+			         'line' => 26,
+			         'name' => 'xyz'
+			       }
+			     ],
+			     'name' => 'main',
+			     'pragmata' => [
+			       {
+			         'line' => 4,
+			         'name' => 'strict'
+			       },
+			       {
+			         'line' => 5,
+			         'name' => 'autodie'
+			       },
+			       {
+			         'line' => 6,
+			         'name' => 'warnings'
+			       },
+			       {
+			         'line' => 8,
+			         'name' => 'lib'
+			       }
+			     ]
+			   }
+			 ]
+	},
+);
+
+plan tests => @cases * 1;
+
+use PPIx::EditorTools::Outline;
+
+foreach my $c (@cases) {
+	my $outline = PPIx::EditorTools::Outline->new->find( code => $c->{code} );
+	#diag explain $outline;
+	is_deeply $outline, $c->{expected};
+}
+
